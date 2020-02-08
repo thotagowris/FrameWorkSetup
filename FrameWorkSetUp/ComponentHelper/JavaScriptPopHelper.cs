153758896ff41dcc527b3402817e1d3acc4825ce
Gowri Thota <thota.gowris@gmail.com>
2020-02-08 21:49:44 +0000

    Added Pop up Accept and Cancel and it's helper, Added Prompt Accept and Cancel, with Text and it's helper

---
+++ FrameWorkSetUp/ComponentHelper/JavaScriptPopHelper.cs
@@ -0,0 +1,56 @@
+﻿using FrameWorkSetUp.Settings;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorkSetUp.ComponentHelper
+{
+    public class JavaScriptPopHelper
+    {
+        public static bool IsPopUpPresent()
+        {
+            try
+            {
+                ObjectRepositiry.Driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+
+                return false;
+            }
+        }
+
+        public static string GetPopUpText()
+        {
+            if (!IsPopUpPresent())
+                return "";
+            return ObjectRepositiry.Driver.SwitchTo().Alert().Text;
+        }
+
+        public static void ClickOnPopUp()
+        {
+            if (!IsPopUpPresent())
+                return;
+            ObjectRepositiry.Driver.SwitchTo().Alert().Accept();
+
+        }
+
+        public static void ClickCancelOnPopup()
+        {
+            if (!IsPopUpPresent())
+                return;
+            ObjectRepositiry.Driver.SwitchTo().Alert().Dismiss();
+        }
+
+        public static void sendKeys(string text)
+        {
+            if (!IsPopUpPresent())
+                return;
+            ObjectRepositiry.Driver.SwitchTo().Alert().SendKeys(text);
+        }
+    }
+}
--- FrameWorkSetUp/TestScript/Popups/TestPopups.cs
+++ FrameWorkSetUp/TestScript/Popups/TestPopups.cs
@@ -24,12 +24,60 @@ namespace FrameWorkSetUp.TestScript.Popups
             Thread.Sleep(2000);
             BrowserHelper.SwitchToFrame(By.Id("iframeResult"));
             ButtonHelper.ClickButton(By.CssSelector("body > button:nth-child(2)"));
-            IAlert alert = ObjectRepositiry.Driver.SwitchTo().Alert();
-            var text = alert.Text;
-            alert.Accept();
+            var text = JavaScriptPopHelper.GetPopUpText();
+            JavaScriptPopHelper.ClickOnPopUp();
+            //IAlert alert = ObjectRepositiry.Driver.SwitchTo().Alert();
+            //var text = alert.Text;
+            //alert.Accept();
             ObjectRepositiry.Driver.SwitchTo().DefaultContent();
             //TextBoxHelper.ClearTextBox(By.Id("textareaCode"));
             //TextBoxHelper.TypeInTextBox(By.Id("textareaCode"), text);
+        }
+
+        [TestMethod]
+        public void TestConfirmPopup()
+        {
+            NavigationHelper.NavigateToUrl("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm");
+            BrowserHelper.SwitchToFrame(By.Id("iframeResult"));
+            ButtonHelper.ClickButton(By.CssSelector("body > button:nth-child(2)"));
+            Thread.Sleep(2000);
+            var text = JavaScriptPopHelper.GetPopUpText();
+            JavaScriptPopHelper.ClickOnPopUp();
+            Thread.Sleep(2000);
+            //IAlert confirm = ObjectRepositiry.Driver.SwitchTo().Alert();
+            //confirm.Accept();
+            ButtonHelper.ClickButton(By.CssSelector("body > button:nth-child(2)"));
+            Thread.Sleep(2000);
+            var text2 = JavaScriptPopHelper.GetPopUpText();
+            Thread.Sleep(2000);
+            JavaScriptPopHelper.ClickCancelOnPopup();
+            Thread.Sleep(2000);
+            ObjectRepositiry.Driver.SwitchTo().DefaultContent();
+            //IAlert reject = ObjectRepositiry.Driver.SwitchTo().Alert();
+            //reject.Dismiss();
+            //TextBoxHelper.ClearTextBox(By.CssSelector(".CodeMirror"));
+            //TextBoxHelper.TypeInTextBox(By.CssSelector(".CodeMirror"), text);
+        }
+
+        [TestMethod]
+        public void TestPrompt()
+        {
+            NavigationHelper.NavigateToUrl("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt");
+            BrowserHelper.SwitchToFrame(By.Id("iframeResult"));
+            ButtonHelper.ClickButton(By.CssSelector("body > button:nth-child(2)"));
+            //IAlert prompt = ObjectRepositiry.Driver.SwitchTo().Alert();
+            JavaScriptPopHelper.sendKeys("This is Automation");
+            JavaScriptPopHelper.ClickOnPopUp();
+            //prompt.SendKeys("This Is Automation");
+            //prompt.Accept();
+            Thread.Sleep(2000);
+            
+            ButtonHelper.ClickButton(By.CssSelector("body > button:nth-child(2)"));
+            JavaScriptPopHelper.sendKeys("This is Automation");
+            JavaScriptPopHelper.ClickCancelOnPopup();
+            //prompt = ObjectRepositiry.Driver.SwitchTo().Alert();
+            //prompt.Dismiss();
+            Thread.Sleep(2000);
         }
     }
 }
